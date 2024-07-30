96283535818e6bb815a5f92b6ee9954d05d2e789
Pankaj Kumar Dhal <pk99dhal@gmail.com>
2024-07-30 13:09:59 +0530

    Dom project 1

---
+++ index.js
@@ -0,0 +1,11 @@
+var divElement =document.getElementById("box");
+
+        divElement.style.backgroundColor ="yellow";
+        divElement.style.color ="blue";
+        divElement.style.margin ="20px";
+        divElement.style.padding ="20px";
+        divElement.style.fontSize ="18px";
+        divElement.style.fontWeight ="bold";
+        divElement.style.height ="200px";
+        divElement.style.width ="300px";
+        divElement.style.height ="200px";
